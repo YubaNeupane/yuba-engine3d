df33d980f7b24f7b983143d976ab226df585409c
Yuba Neupane <neopaneyb2@gmail.com>
2025-12-05 20:38:13 -0500

    small fix

--- src/main/java/Engine/Core/Render/Render.java
+++ src/main/java/Engine/Core/Render/Render.java
@@ -28,7 +28,7 @@ public class Render {
 
     public void render(Window window, Scene scene){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glViewport(0,0, window.getWidth(), window.getHeight());
+        glViewport(0,0, Window.getWidth(), Window.getHeight());
 
         sceneRender.render(scene);
     }
